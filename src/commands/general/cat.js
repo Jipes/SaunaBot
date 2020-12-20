aac8d36c9b58836bc168684ad8913c074b6ef27f
-Jipes <55793183+Jipes@users.noreply.github.com>
2020-12-20 19:05:39 +0200

    Update cat.js

--- src/commands/general/cat.js
+++ src/commands/general/cat.js
@@ -9,7 +9,9 @@ module.exports = {
     async execute(message, args, client, db) {
     
     // Main
-    let body = await bent("GET", 200, "json", {"x-api-key": "sensored"})("https://api.thecatapi.com/v1/images/search")
+    client.config = require('../../config.json');
+    let body = await bent("GET", 200, "json", {"x-api-key": client.config.animalapi})("https://api.thecatapi.com/v1/images/search")
+
 
     // Embed
         const Cat = new MessageEmbed()
